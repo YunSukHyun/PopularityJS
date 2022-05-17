e424d3ce9dca7d14f501c551459c360f0336f711
YunSukHyun <czcz9207@gmail.com>
2022-05-17 18:42:24 +0900

    second-commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const db_config = {
     host:'localhost',
     port:3306,
     user:'root',
-    password:'sk5769',
+    password:'',
     database:'priconne'
 };
 const conn = maria.createConnection(db_config);
